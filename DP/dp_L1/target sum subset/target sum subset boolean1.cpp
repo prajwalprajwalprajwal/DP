7f7b5e876553be50d1c1fa3b16bff99d9f258446
prajwalprajwalprajwal <89747349+prajwalprajwalprajwal@users.noreply.github.com>
2022-04-07 00:41:53 +0530

    Create target sum subset boolean1.cpp

---
+++ DP/dp_L1/target sum subset/target sum subset boolean1.cpp
@@ -0,0 +1,49 @@
+//little bit error
+
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args)throws Exception{
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=scn.nextInt();
+        }
+        int target;
+        target = scn.nextInt();
+        boolean[][] strgs = new boolean[arr.length+1][target+1];
+        boolean sets = subsets(arr, target,0,strgs);
+        System.out.println(sets);
+    }
+                                         
+    public static boolean subsets(int[] arr, int target, int idx, boolean[][] strgs)
+    {
+        
+        if(target==0)
+        {
+            
+            return true;
+        }
+        else if(idx==arr.length)
+        {
+            
+            return false;
+        }
+        
+        if(target>0 && strgs[idx][target]!=null)
+        {
+            return strgs[idx][target];
+        }
+        boolean include = subsets(arr, target-arr[idx],idx +1, strgs);
+        boolean exclude = subsets(arr, target,idx+1, strgs);
+        
+        boolean ans = include || exclude;
+        if(target>0)
+        strgs[idx][target]=ans;
+        return ans;
+    }
+}
