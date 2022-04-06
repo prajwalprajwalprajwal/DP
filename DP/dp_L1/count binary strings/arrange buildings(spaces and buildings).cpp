2286ab0bf92a62fe2ceff868887036ace7b58c68
prajwalprajwalprajwal <89747349+prajwalprajwalprajwal@users.noreply.github.com>
2022-04-07 02:06:37 +0530

    Create arrange buildings(spaces and buildings).cpp

---
+++ DP/dp_L1/count binary strings/arrange buildings(spaces and buildings).cpp
@@ -0,0 +1,29 @@
+//count binary strings
+//https://nados.io/question/arrange-buildings
+// 4th april - sumit sir
+
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args)throws Exception{
+       Scanner scn = new Scanner(System.in);
+       int n = scn.nextInt();
+        
+        long count=0;
+        int[][] arr = new int[2][n+1];
+        arr[0][0]=0;
+        arr[1][0]=0;
+        arr[0][1]=1;
+        arr[1][1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            arr[0][i]=arr[1][i-1];
+            arr[1][i]=arr[0][i-1]+arr[1][i-1];
+        }
+        count=arr[0][n]+arr[1][n];
+        count=count*count;
+        System.out.println(count);
+       
+    }
+}
